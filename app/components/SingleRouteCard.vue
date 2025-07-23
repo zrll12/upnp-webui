74ac8dbdfbf80ab8e36b2da8bd5a8a65ff61db52
zrll <46812903+zrll12@users.noreply.github.com>
2025-07-23 22:04:10 +0800

    feat: impl route list

---
+++ app/components/SingleRouteCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{ route: Routes }>()
+
+const titleStr = `${props.route.outerPort}:${props.route.innerPort}`
+const created = computed(() => new Date(props.route.createdAt).toLocaleString())
+const updated = computed(() => new Date(props.route.updatedAt).toLocaleString())
+</script>
+
+<template>
+<n-card :title="titleStr" class="w-80">
+  <div class="flex flex-col gap-2">
+    <n-text>{{ props.route.name }}</n-text>
+    <n-text class="text-gray-500">ID: {{ props.route.id }}</n-text>
+    <n-text class="text-gray-500">创建时间: {{ created }}</n-text>
+    <n-text class="text-gray-500">更新时间: {{ updated }}</n-text>
+  </div>
+</n-card>
+</template>
+
+<style scoped>
+
+</style>
