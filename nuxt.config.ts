754643b4228dad547bae3ead26b6a46c06ae14a4
zrll <46812903+zrll12@users.noreply.github.com>
2025-07-23 14:50:00 +0800

    feat: add naiveui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
+	modules: ["nuxtjs-naive-ui"],
+	vite: {
+		plugins: [
+			AutoImport({
+				imports: [
+					{
+						"naive-ui": [
+							"useDialog",
+							"useMessage",
+							"useNotification",
+							"useLoadingBar",
+						],
+					},
+				],
+			}),
+			Components({
+				resolvers: [NaiveUiResolver()],
+			}),
+		],
+	},
 });
